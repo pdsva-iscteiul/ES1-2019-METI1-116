4e4c5eddb7d0de3ab00d88d76e78bb7ed6b4477c
daepe <daepe@iscte-iul.pt>
2019-11-14 00:47:22 +0000

    First Version of the excel reader

---
+++ ProjectEngSoft/src/ExcelReader/Reader.java
@@ -0,0 +1,47 @@
+package ExcelReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Reader {
+
+	private Workbook wb;
+	private Sheet sh;
+	private FileInputStream excelFile;
+
+
+	public Reader(File f) throws IOException {
+		excelFile = new FileInputStream(f);
+		wb = new XSSFWorkbook(excelFile);
+		sh = wb.getSheetAt(0);
+	}
+
+	public String [][] read(){
+		DataFormatter formatter= new DataFormatter();
+		String [][] aux = null;
+		Row row= sh.getRow(sh.getFirstRowNum());
+		int max, col=0;
+		for(int i = 0;i != sh.getLastRowNum(); i++) {
+			row = sh.getRow(i);
+			if(row != null) {
+				max = sh.getRow(i).getPhysicalNumberOfCells();
+				if(max > col) col = max;
+			}
+		}
+
+		aux= new String[sh.getPhysicalNumberOfRows()][col];
+		for(int i=0; i!=aux.length; i++) {
+			for(int j=0;j!=aux[i].length;j++) {
+				aux[i][j]= formatter.formatCellValue(sh.getRow(i).getCell(j));
+			}
+		}
+		return aux;
+	}
+}
