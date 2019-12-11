abfe301f1ad0ff48117d2df0191efe67a1077db6
sadfo-iscteiul <sadfo@iscte-iul.pt>
2019-12-11 23:34:58 +0000

    PMD and iPlasma code finalization

--- ProjectEngSoft/src/AuxPackage/Reader.java
+++ ProjectEngSoft/src/AuxPackage/Reader.java
@@ -3,6 +3,7 @@ package AuxPackage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -11,12 +12,17 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openxmlformats.schemas.drawingml.x2006.chart.impl.STRadarStyleImpl;
 
 public class Reader {
 
 	private Workbook wb;
 	private Sheet sh;
 	private FileInputStream excelFile;
+	private int DCI;
+	private int ADCI;
+	private int DII;
+	private int ADII;
 
 
 	public Reader(File f) throws IOException {
@@ -25,6 +31,66 @@ public class Reader {
 		sh = wb.getSheetAt(0);
 	}
 
+	public static void main(String[] args)  {
+		Reader r ;
+		try {
+			r = new Reader(new File ("C:/Users/lenovo 530S-14IKB/Desktop/Long-Method.xlsx"));
+			r.evaluate("iPlasma");
+			System.out.println(r.ADCI);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public  ArrayList<String> evaluate(String a) {
+		ArrayList<String> s = new ArrayList<String>();
+		DCI = 0;
+		DII = 0;
+		ADCI = 0;
+		ADII = 0;
+		for (int i = 1; i!= getValue(a).size()+1; i++ ) {
+			if (getValue(a).get(i).toString().equals("TRUE") && getValue("is_long_method").get(i).toString().equals("TRUE")) {
+				s.add("DCI");
+				DCI++;
+			}
+			if (getValue(a).get(i).toString().equals("TRUE") && getValue("is_long_method").get(i).toString().equals("FALSE")) {
+				s.add("DII");
+				DII++;
+			}
+			if (getValue(a).get(i).toString().equals("FALSE") && getValue("is_long_method").get(i).toString().equals("FALSE")) {
+				s.add("ADCI");
+				ADCI++;
+			}
+			if (getValue(a).get(i).toString().equals("FALSE") && getValue("is_long_method").get(i).toString().equals("TRUE")) {
+				s.add("ADII");
+				ADII++;
+			}
+		}
+		return s;
+	}
+
+
+
+	public int getDCI() {
+		return DCI;
+	}
+
+
+	public int getADCI() {
+		return ADCI;
+	}
+
+
+	public int getDII() {
+		return DII;
+	}
+
+	public int getADII() {
+		return ADII;
+	}
+
+	
 	public String [][] read(){
 		DataFormatter formatter= new DataFormatter();
 		String [][] aux = null;
@@ -46,11 +112,11 @@ public class Reader {
 		}
 		return aux;
 	}
-	
+
 	public HashMap<Integer, Cell> getValue(String metric){
 		HashMap<Integer, Cell> aux= new HashMap<Integer, Cell>();
 		Row row= sh.getRow(sh.getFirstRowNum());
-		for(int i=1; i!=row.getLastCellNum(); i++) {
+		for(int i=0; i!=row.getLastCellNum(); i++) {
 			if(metric.equals(row.getCell(i).getStringCellValue())) {
 				for(int j=1;j!=sh.getLastRowNum()+1;j++) {
 					aux.put((int)sh.getRow(j).getCell(0).getNumericCellValue(), sh.getRow(j).getCell(i));
@@ -59,5 +125,5 @@ public class Reader {
 		}
 		return aux;
 	}
-	
+
 }
