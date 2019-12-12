7b7fe6600001c6079a5ca321e37b41ddf9ab986e
jpmrf-iscteiul <jpmrf@iscte-iul.pt>
2019-12-12 21:47:31 +0000

    ReaderTest done.

--- ProjectEngSoft/src/tests/ReaderTest.java
+++ ProjectEngSoft/src/tests/ReaderTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -36,7 +37,8 @@ public class ReaderTest {
 	private String [][] excel;
 	private HashMap <Integer,Cell> hMapMetric; 
 	private HashMap <Integer,Cell> hMapMethod; 
-	private Rule rule;
+	private Rule rule1;
+	private Rule rule2;
 	private Workbook wb;
 	private Sheet sh;
 	private FileInputStream excelFile;
@@ -59,7 +61,8 @@ public class ReaderTest {
 		
 		hMapMetric = reader.getValue("LOC"); // e.g, LOC method
 		hMapMethod = reader.getValue("method");
-		rule = new Rule("joao","AND","LOC = 40");
+		rule1 = new Rule("joao","is_long_method"," ( LOC = 40 ) ");
+		rule2 = new Rule("jesus","is_feature_envy"," ( CYCLO > 3 ) ");
 		excelFile = new FileInputStream(file);
 		wb = new XSSFWorkbook(excelFile);
 		sh = wb.getSheetAt(0);
@@ -68,6 +71,7 @@ public class ReaderTest {
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
@@ -85,37 +89,84 @@ public class ReaderTest {
 	}
 	@Test
 	public void evaluateTest() {
-	assertEquals(reader.evaluate("LOC") );
+		ArrayList<String> s = reader.evaluate("iPlasma");
+		ArrayList<String> m = reader.evaluate("PMD");
+		assertEquals(s.get(0),"ADCI");
+		assertNotEquals(s.get(0),"hello");
+		assertEquals(m.get(0),"ADCI");
+		assertNotEquals(m.get(0),"hello");
+		boolean a = false;
+		boolean b = false;
+		for(int i = 0; i != s.size();i++) {
+			if(s.get(i).equals("ADII")) 
+				a = true;
+				
+			
+			if(m.get(i).equals("ADII"))
+				b=true;
+			
+		}
+		assertFalse(a);
+		assertFalse(b);
+		
 		
 		
 	}
 	@Test
 	public void evaluate1Test() {
-	// System.out.println(rule.compare(sh.getRow(1) && getValue(rule.getType().get(1).getBooleanCellValue()));
+		ArrayList<String> s = reader.evaluate1(rule1);
+		ArrayList<String> m = reader.evaluate1(rule2);
+		assertEquals(s.get(0),"ADCI");
+		assertNotEquals(s.get(0),"hello");
+		assertEquals(m.get(6),"ADII");
+		assertNotEquals(m.get(6),"hello");
 	
 		
 	}
 	@Test
 	public void getDCITest() {
-		for(int i = 1; i<=421;i++) {
-			for(int j  = 7;j<=10;j++) {
-				
-			}
-	}
+		
+		reader.evaluate("PMD");
+		assertNotEquals(reader.getDCI(),2);
+		assertEquals(reader.getADCI(),262);
+
+		reader.evaluate("iPlasma");
+		assertNotEquals(reader.getDCI(),2);
+		assertEquals(reader.getADCI(),280);
 	}
 
 	@Test
 	public void getADCITest() {
+		reader.evaluate("PMD");
+		assertNotEquals(reader.getADCI(),2);
+		assertEquals(reader.getADCI(),262);
 		
-	}
+		reader.evaluate("iPlasma");
+		assertNotEquals(reader.getADCI(),2);
+		assertEquals(reader.getADCI(),280);
+		}
+	
 
 	@Test
 	public void getDIITest() {
-	
+		reader.evaluate("PMD");	
+		assertNotEquals(reader.getDII(),2);
+		assertEquals(reader.getDII(),18);
+		
+		reader.evaluate("iPlasma");
+		assertNotEquals(reader.getDII(),2);
+		assertEquals(reader.getDII(),0);
 	}
+	
 	@Test
 	public void getADIITest() {
+		reader.evaluate("PMD");
+		assertNotEquals(reader.getADII(),2);
+		assertEquals(reader.getADII(),0);
 		
+		reader.evaluate("iPlasma");
+		assertNotEquals(reader.getADII(),2);
+		assertEquals(reader.getADII(),0);
 	}
 
 
