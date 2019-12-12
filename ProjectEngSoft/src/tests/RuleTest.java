4b9cb5943f2d0aebcb9c55b1d6f77fec18fe923a
jpmrf-iscteiul <jpmrf@iscte-iul.pt>
2019-12-12 18:05:10 +0000

    Testing all methods in Rule class except method compare.

--- ProjectEngSoft/src/tests/RuleTest.java
+++ ProjectEngSoft/src/tests/RuleTest.java
@@ -2,20 +2,34 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import AuxPackage.Reader;
+import AuxPackage.Rule;
+
 class RuleTest {
 
 	private ArrayList<String> rulecomponentes;
-	private String name;
+	private String name1;
+	private String name2;
 	private String TypeOfComparation1;
 	private String TypeOfComparation2;
+	private String subrule1;
+	private String subrule2;
+	private String subrule3;
+	private Rule rule1;
+	private Rule rule2;
+	private Row row;
+	private Reader reader;
+	private String[][] matriz;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -29,7 +43,15 @@ class RuleTest {
 	void setUp() throws Exception {
 		TypeOfComparation1  = "OR";
 		TypeOfComparation2 = "AND";
-		name = "jesus";
+		name1 = "jesus";
+		name2 = "joao";
+		subrule1 = "LOC = 40";
+		subrule2 = "CYCLO > 40";
+		rule1 = new Rule(name1,TypeOfComparation1,subrule1);
+		rule2 = new Rule(name2,TypeOfComparation2,subrule2);
+		reader = new Reader(new File("C:/Users/joao/Downloads/Long-Method.xlsx"));
+		matriz=reader.read();
+		// row = (Row) (matriz[1][0]).;
 
 
 	}
@@ -40,28 +62,32 @@ class RuleTest {
 
 	@Test
 	void testRule() {
-		fail("Not yet implemented");
+		System.out.println("This test ran");
 	}
 
 	@Test
 	void testGetRulecomponentes() {
-		fail("Not yet implemented");
+		System.out.println(rule1.getRulecomponentes());
+		assertEquals(rule1.getRulecomponentes().toString(),"[LOC= 40]");
+		assertEquals(rule2.getRulecomponentes().toString(),"[CYCLO> 40]");
 	}
 
 	@Test
 	void testGetName() {
-	//	assertEquals();
-		
+		assertEquals(rule1.getName(),"jesus");
+		assertEquals(rule2.getName(),"joao");
+	
 	}
 
 	@Test
 	void testGetType() {
-		fail("Not yet implemented");
+		assertEquals(rule1.getType(),"OR");
+		assertEquals(rule2.getType(),"AND");
 	}
 
 	@Test
 	void testCompare() {
-		fail("Not yet implemented");
+		
 	}
 
 }
