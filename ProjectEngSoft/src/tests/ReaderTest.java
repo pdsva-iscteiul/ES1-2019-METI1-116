0c7d429cfe0b8be831cb5d6f31306cc0c64794a3
jpmrf-iscteiul <jpmrf@iscte-iul.pt>
2019-12-12 19:33:01 +0000

    Improvements in ReaderTest class

--- ProjectEngSoft/src/tests/ReaderTest.java
+++ ProjectEngSoft/src/tests/ReaderTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -26,6 +27,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import AuxPackage.Reader;
+import AuxPackage.Rule;
 
 public class ReaderTest {
 
@@ -34,6 +36,10 @@ public class ReaderTest {
 	private String [][] excel;
 	private HashMap <Integer,Cell> hMapMetric; 
 	private HashMap <Integer,Cell> hMapMethod; 
+	private Rule rule;
+	private Workbook wb;
+	private Sheet sh;
+	private FileInputStream excelFile;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -47,12 +53,17 @@ public class ReaderTest {
 	@Before
 	public void setUp() throws Exception {
 		
-		file = new File("/Users/pedrov510/Desktop/Uni/ES/Long-Method.xlsx"); // only for Long-Method excel file
+		file = new File("C:/Users/joao/Downloads/Long-Method.xlsx"); // only for Long-Method excel file
 		reader = new Reader(file);
 		excel = reader.read();	
 		
 		hMapMetric = reader.getValue("LOC"); // e.g, LOC method
 		hMapMethod = reader.getValue("method");
+		rule = new Rule("joao","AND","LOC = 40");
+		excelFile = new FileInputStream(file);
+		wb = new XSSFWorkbook(excelFile);
+		sh = wb.getSheetAt(0);
+		
 	}
 
 	@After
@@ -71,6 +82,40 @@ public class ReaderTest {
 		assertEquals(hMapMetric.get(1).toString(),"3.0");
 		assertEquals(hMapMethod.get(1).toString(),"Output()");
 		assertNotEquals(hMapMethod.get(1).toString(),"Result()");
+	}
+	@Test
+	public void evaluateTest() {
+	assertEquals(reader.evaluate("LOC") );
+		
+		
+	}
+	@Test
+	public void evaluate1Test() {
+	// System.out.println(rule.compare(sh.getRow(1) && getValue(rule.getType().get(1).getBooleanCellValue()));
+	
+		
+	}
+	@Test
+	public void getDCITest() {
+		for(int i = 1; i<=421;i++) {
+			for(int j  = 7;j<=10;j++) {
+				
+			}
+	}
+	}
+
+	@Test
+	public void getADCITest() {
+		
+	}
+
+	@Test
+	public void getDIITest() {
+	
+	}
+	@Test
+	public void getADIITest() {
+		
 	}
 
 
