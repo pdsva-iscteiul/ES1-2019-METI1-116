84fc2a7eae6a3a49e4918e8c34d9e0dc17a3ed89
Filipe <Filipe@192.168.1.67>
2019-12-12 16:55:26 +0000

    Fixing conflicts in classpath and readerTest

--- ProjectEngSoft/src/tests/ReaderTest.java
+++ ProjectEngSoft/src/tests/ReaderTest.java
@@ -3,7 +3,6 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.io.File;
-<<<<<<< HEAD
 import java.io.FileInputStream;
 import java.util.HashMap;
 
@@ -16,57 +15,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ExcelReader.Reader;
 
-// this test is considering the "Long-Method.xml" file
-
-public class ReaderTest {
-
-	private Reader reader;
-	private File file;
-	private String [][] excel;
-	private HashMap <Integer,Cell> hMapMetric; 
-	private HashMap <Integer,Cell> hMapMethod; 
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("ReaderTest JUnit");
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		
-		file = new File("/Users/pedrov510/Desktop/Uni/ES/Long-Method.xlsx"); // only for Long-Method excel file
-		reader = new Reader(file);
-		excel = reader.read();	
-		
-		hMapMetric = reader.getValue("LOC"); // e.g, LOC method
-		hMapMethod = reader.getValue("method");
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void readTest() {	
-		String[][] matrix = new String [1][2];
-		assertNotEquals(excel,matrix);
-	}
-
-	@Test
-	public void getValueTest() {	
-		assertNotEquals(hMapMetric.get(1).toString(),"5.0");
-		assertEquals(hMapMetric.get(1).toString(),"3.0");
-		assertEquals(hMapMethod.get(1).toString(),"Output()");
-		assertNotEquals(hMapMethod.get(1).toString(),"Result()");
-	}
-	
-=======
 import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
