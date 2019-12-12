98c5fd03515d77d97109692a976e68c4ba423f2e
pdsva-iscteiul <pdsva@iscte-iul.pt>
2019-12-12 23:20:10 +0000

    Make JUnit test for Rule class.

--- ProjectEngSoft/src/tests/RuleTest.java
+++ ProjectEngSoft/src/tests/RuleTest.java
@@ -17,19 +17,30 @@ import AuxPackage.Rule;
 
 class RuleTest {
 
-	private ArrayList<String> rulecomponentes;
 	private String name1;
 	private String name2;
+	private String name3;
 	private String TypeOfComparation1;
 	private String TypeOfComparation2;
 	private String subrule1;
 	private String subrule2;
 	private String subrule3;
+	private String subrule4;
+	private String subrule5;
+	private String subrule6;
+	private String subrule7;
+	private String subrule8;
 	private Rule rule1;
 	private Rule rule2;
-	private Row row;
+	private Rule rule3;
+	private Rule rule4;
+	private Rule rule5;
+	private Rule rule6;
+	private Rule rule7;
+	private Row row1;
+	private Row row2;
+	private Row row3;
 	private Reader reader;
-	private String[][] matriz;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -41,19 +52,45 @@ class RuleTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		TypeOfComparation1  = "OR";
-		TypeOfComparation2 = "AND";
+		
+		TypeOfComparation1  = "is_long_method";
+		TypeOfComparation2 = "is_feature_envy";
+		
 		name1 = "jesus";
 		name2 = "joao";
-		subrule1 = "LOC = 40";
-		subrule2 = "CYCLO > 40";
-		rule1 = new Rule(name1,TypeOfComparation1,subrule1);
-		rule2 = new Rule(name2,TypeOfComparation2,subrule2);
-		reader = new Reader(new File("C:/Users/joao/Downloads/Long-Method.xlsx"));
-		matriz=reader.read();
-		// row = (Row) (matriz[1][0]).;
+		name3 = "dave";
+		
+		subrule1 = " ( LOC = 5 )";
+		subrule2 = "( LOC < 20 )";
+		subrule3 = " ( LOC > 1 )";
+		
+		subrule4 = " ( CYCLO = 1 )";
+		subrule5 = "( CYCLO < 30 )";
+		subrule6 = " ( CYCLO > 2 )";
+		
+		subrule7 = " ( LAA = 0 )"; 
+		subrule8 = "( ATFD < 10 )";
+		
+		String [] cond1 = {subrule6," AND ",subrule2}; // CYCLO>1 AND LOC<20
+		String [] cond2 = {subrule3," OR ",subrule5}; // LOC > 1 OR CYCLO<30
+		
+		String [] cond3 = {subrule7," AND ",subrule8}; // LAA > 0 AND ATFD<50
+		
+		rule1 = new Rule(name1,TypeOfComparation1,subrule1); // is_long_method
+		rule3 = new Rule(name3,TypeOfComparation1,subrule3); // is_long_method
+		rule4 = new Rule(name1,TypeOfComparation1,subrule4); // is_long_method
+		rule6 = new Rule(name1,TypeOfComparation1,cond1); // is_long_method
+		rule7 = new Rule(name2,TypeOfComparation1, cond2); // is_long_method
+		
+		rule2 = new Rule(name2,TypeOfComparation2,cond3); // feature_envy
+		//rule5 = new Rule(name3,TypeOfComparation2,subrule8); // feature_envy
 
-
+		// load the excel file
+		reader = new Reader(new File("/Users/pedrov510/Desktop/Uni/ES/Long-Method.xlsx"));
+		
+		row1 = reader.getSh().getRow(3); //e.g, row number 3
+		row2 = reader.getSh().getRow(25); // for testing cond1
+		row3 = reader.getSh().getRow(22); // for testing cond2		
 	}
 
 	@AfterEach
@@ -67,27 +104,65 @@ class RuleTest {
 
 	@Test
 	void testGetRulecomponentes() {
-		System.out.println(rule1.getRulecomponentes());
-		assertEquals(rule1.getRulecomponentes().toString(),"[LOC= 40]");
-		assertEquals(rule2.getRulecomponentes().toString(),"[CYCLO> 40]");
+		assertEquals(rule1.getRulecomponentes().toString(),"[( LOC = 5 )]");
+		assertEquals(rule4.getRulecomponentes().toString(),"[( CYCLO = 1 )]");
+		//assertEquals(rule2.getRulecomponentes().toString(),"[( LAA > 0 )]");
 	}
 
 	@Test
 	void testGetName() {
 		assertEquals(rule1.getName(),"jesus");
-		assertEquals(rule2.getName(),"joao");
+		assertEquals(rule7.getName(),"joao");
 	
 	}
 
 	@Test
 	void testGetType() {
-		assertEquals(rule1.getType(),"OR");
-		assertEquals(rule2.getType(),"AND");
+		assertEquals(rule1.getType(),"is_long_method");
+		assertEquals(rule3.getType(),"is_long_method");
 	}
 
 	@Test
 	void testCompare() {
 		
+		boolean expected1 = rule1.compare(row1); // should return true, because LOC=5 in row 3
+		assertTrue(expected1);
+		assertFalse(rule1.compare(row2));
+		
+		boolean expected2 = rule3.compare(row1); // should return true
+		assertTrue(expected2);
+		
+		boolean expected4 = rule4.compare(row1); //should return 1, because CYCLO=1
+		assertTrue(expected4);
+		assertFalse(rule4.compare(row2));
+		
+		boolean expected6 = rule6.compare(row2);
+		assertTrue(expected6);
+		assertFalse(rule6.compare(row1));
+		
+		boolean expected7 = rule7.compare(row3);
+		assertTrue(expected7);
+			
+		boolean expected8 = rule2.compare(row3);
+		assertTrue(expected8);
+		
 	}
+	
+/*
+	@Test
+	void testAndOr() {
+		
+	ArrayList<Boolean> booleanlist = new ArrayList<>();
+	ArrayList<String> operatorlist = new ArrayList<>();
+	booleanlist.add(true);	
+	booleanlist.add(true);
+	
+	operatorlist.add(" AND ");
 
+	boolean expected = rule1.getAndOr(booleanlist, operatorlist);
+	
+	assertTrue(expected);
+	}
+*/	
+	
 }
